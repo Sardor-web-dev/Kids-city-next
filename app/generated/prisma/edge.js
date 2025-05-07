f33bd0c40924785213d34602f29c4e4fe1feff20
SarikViper <dzamolovsardor5@gmail.com>
2025-05-07 19:55:27 +0500

    deleted eslint

--- app/generated/prisma/edge.js
+++ app/generated/prisma/edge.js
@@ -169,11 +169,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:Sardor%202010@localhost:5432/kidscity?schema=public"
+        "value": null
       }
     }
   },
