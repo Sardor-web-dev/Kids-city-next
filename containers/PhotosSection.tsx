91980962dac21227c770d22635844bdca855faf9
SarikViper <dzamolovsardor5@gmail.com>
2025-05-11 21:32:37 +0500

    done slider

--- containers/PhotosSection.tsx
+++ containers/PhotosSection.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Carousel,
   CarouselContent,
@@ -5,12 +6,16 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-
+import Autoplay from "embla-carousel-autoplay";
 const PhotosSection = () => {
   const images = [
     { src: "/inside.jpg", alt: "Интерьер магазина" },
     { src: "/polka.jpg", alt: "Полка с товарами" },
-    { src: "/main.jpg", alt: "Фасад магазина" },
+    { src: "/main.jpg", alt: "Фотография внутри" },
+    { src: "/paijamas.jpg", alt: "Фото продукции" },
+    { src: "/clothes.jpg", alt: "Фото одежды" },
+    { src: "/shop.png", alt: "Фото снаружи" },
+    { src: "/jeans.jpg", alt: "Фото брюков" },
   ];
 
   return (
@@ -22,19 +27,50 @@ const PhotosSection = () => {
         </span>
       </div>
 
-      <div className="lg:flex hidden lg:flex-row gap-6">
-        {images.map((img, i) => (
-          <img
-            key={i}
-            className="w-90 rounded-2xl h-100 hover:scale-105 transition-all cursor-pointer lg:opacity-90 hover:opacity-100"
-            src={img.src}
-            alt={img.alt}
-          />
-        ))}
+      <div className="lg:flex mt-5 hidden lg:flex-row gap-6">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          opts={{
+            align: "start", 
+            slidesToScroll: 1, 
+          }}
+          className="relative flex flex-col items-center justify-center w-full"
+        >
+          <CarouselContent>
+            {images.map((img, i) => (
+              <CarouselItem
+                key={i}
+                className="basis-1/3 w-full flex items-center max-w-[1200px]"
+              >
+                <img
+                  className="w-[400px] h-[400px] object-cover rounded-2xl hover:scale-105 transition-all cursor-pointer"
+                  src={img.src}
+                  alt={img.alt}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          <div className="flex justify-center gap-1 mt-10">
+            <CarouselPrevious className="cursor-pointer" />
+            <CarouselNext className="cursor-pointer" />
+          </div>
+        </Carousel>
       </div>
 
       <div className="lg:hidden flex flex-col items-center">
-        <Carousel className="relative flex flex-col items-center justify-center w-full max-w-xs">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          className="relative flex flex-col items-center justify-center w-full max-w-xs"
+        >
           <CarouselContent>
             {images.map((img, i) => (
               <CarouselItem key={i} className="flex justify-center">
@@ -47,9 +83,9 @@ const PhotosSection = () => {
             ))}
           </CarouselContent>
 
-          <div className="flex justify-center gap-1 mt-10">
-            <CarouselPrevious className="relative static cursor-pointer" />
-            <CarouselNext className="relative static cursor-pointer" />
+          <div className="flex relative justify-center mt-10">
+            <CarouselPrevious className="cursor-pointer" />
+            <CarouselNext className="cursor-pointer" />
           </div>
         </Carousel>
       </div>
