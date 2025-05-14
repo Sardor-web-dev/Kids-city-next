4e18125fb0da28d3637c5038c657e0b7a272f817
SarikViper <dzamolovsardor5@gmail.com>
2025-05-14 22:28:16 +0500

    added jest library for testing

---
+++ __tests__/Exapmple.test.tsx
@@ -0,0 +1,9 @@
+import Example from "@/components/custom/Example";
+import { render, screen } from "@testing-library/react";
+
+describe("Example component", () => {
+  it("renders hello world text", () => {
+    render(<Example />);
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
+  });
+});
---
+++ components/custom/Example.tsx
@@ -0,0 +1,11 @@
+function Example() {
+    return (
+        <>
+        <div>
+            <p>Hello World</p>
+        </div>
+        </>
+    );
+}
+
+export default Example;
