4ec217055dfdcd721e21e06e9ef0405ee9989f46
SarikViper <dzamolovsardor5@gmail.com>
2025-05-16 17:29:03 +0500

    done cart page and order features

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
--- app/api/cloth/route.ts
+++ app/api/cloth/route.ts
@@ -1,28 +1,32 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST(request: Request) {
-  const { name, description, Image, gender } = await request.json();
+  const { name, description, Image, gender, price } = await request.json();
 
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  const admin: any = await prisma.admin.findUnique({
+  const admin = await prisma.admin.findUnique({
     where: {
       email: session.user?.email || "",
     },
   });
+
   if (!admin) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
 
-  if (!name || !description) {
+  if (!name || !description || !Image || !gender || !price) {
     return NextResponse.json(
-      { error: "Title and content are required" },
+      {
+        error: "Все поля обязательны: name, description, Image, gender, price",
+      },
       { status: 400 }
     );
   }
@@ -34,7 +38,12 @@ export async function POST(request: Request) {
         description,
         Image,
         gender,
-        authorId: admin.id,
+        price,
+        author: {
+          connect: {
+            id: admin.id,
+          },
+        },
       },
     });
 
@@ -42,7 +51,7 @@ export async function POST(request: Request) {
   } catch (error) {
     console.error("Server error:", error);
     return NextResponse.json(
-      { error: "Failed to create post" },
+      { error: "Failed to create cloth item" },
       { status: 500 }
     );
   }
