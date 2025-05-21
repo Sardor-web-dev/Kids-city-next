fcc7aaadcadeb056881e198fe34056c314a4d4ce
SarikViper <dzamolovsardor5@gmail.com>
2025-05-21 23:20:06 +0500

    fixed bugs

--- app/catalogue/page.tsx
+++ app/catalogue/page.tsx
@@ -1,16 +1,16 @@
-import AnimatedCatalogue from "@/components/animations/AnimatedCatalogue";
 import prisma from "@/lib/prisma";
 import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
+import AnimatedCatalogue from "@/components/animations/AnimatedCatalogue";
 
 type Props = {
-  searchParams: {
-    gender?: string;
-  };
+  searchParams: Promise<{ gender?: string }>; // Promise, так как Next.js требует
 };
 
 export default async function Catalogue({ searchParams }: Props) {
-  const gender = searchParams.gender;
+  const params = await searchParams; // ждем объект параметров
+  const gender = params.gender;
+
   if (!gender || !["boy", "girl"].includes(gender)) {
     return notFound();
   }
