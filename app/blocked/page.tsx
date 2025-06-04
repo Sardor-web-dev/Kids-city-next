bc13be62ec217d1c1ffc12223006986d0d42ec86
SarikViper <dzamolovsardor5@gmail.com>
2025-06-04 22:53:00 +0500

    done

--- app/blocked/page.tsx
+++ app/blocked/page.tsx
@@ -3,6 +3,7 @@ export default function BlockedPage() {
     <div className="p-10 text-center">
       <h1 className="text-2xl font-bold">Аккаунт заблокирован</h1>
       <p>Пожалуйста, свяжитесь с администрацией.</p>
+      <p>После разблокировки перелогинтесь на страницу профиля чтобы мы обновили вам токен</p>
     </div>
   );
 }
