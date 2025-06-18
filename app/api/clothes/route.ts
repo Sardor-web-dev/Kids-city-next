c62f8c0c8774937873ccef11c833224c8f93fc5b
SarikViper <dzamolovsardor5@gmail.com>
2025-06-18 17:01:46 +0500

    created api route for android app

---
+++ app/api/clothes/route.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, { params }: any) {
+  try {
+ 
+    const cloth = await prisma.cloth.findMany();
+
+    if (!cloth) {
+      return NextResponse.json({ error: "Товар не найден" }, { status: 404 });
+    }
+
+    return NextResponse.json(cloth);
+  } catch (error) {
+    console.error("Ошибка получения товара:", error);
+    return NextResponse.json(
+      { error: "Ошибка получения товара" },
+      { status: 500 }
+    );
+  }
+}
